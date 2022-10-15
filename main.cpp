2c6dce8360cc1d1405f7eb6805622e8071846b2d
gilleece <seangilleece@gmail.com>
2022-10-15 12:05:15 +0100

    Added background layer, fixed bug in bg layer scroll loop

--- main.cpp
+++ main.cpp
@@ -2,36 +2,39 @@
 
 int main()
 {
+    int winHeight = 1024;
+    int winWidth = 600;
     // Create game window
-    InitWindow(600, 1024, "Space Ex");
+    InitWindow(winWidth, winHeight, "Space Ex");
     // Lock framerate to monitor rate
     SetWindowState(FLAG_VSYNC_HINT);
 
     //Load textures for background layers
     Texture2D background = LoadTexture("assets/backgrounds/bg1.png");
-    Texture2D starLayer = LoadTexture("assets/backgrounds/ol3.png");
+    Texture2D starLayer = LoadTexture("assets/backgrounds/ol4.png");
 
-    float bgScroll = -background.height;
-    float midScroll = -starLayer.height;
+    float bgScroll = 0;
+    float midScroll = -starLayer.height*2;
+    float hozScroll = 0;
 
     //Game loop
     while (!WindowShouldClose())
     {
         //Update background positions
-        bgScroll += 20.0f;
+        bgScroll += 5.0f;
         midScroll += 20.0f;
 
 
         //Reset background position to loop
         if (bgScroll >= background.height*2) bgScroll = 0;
-        if (midScroll >= starLayer.height) midScroll = 0;
+        if (midScroll >= starLayer.height*2) midScroll = -starLayer.height*2;
 
         BeginDrawing();
             ClearBackground(BLACK);
 
             //Draw backgrounds based on positions being updated
-            DrawTextureEx(background, (Vector2){ 0, bgScroll }, 0.0f, 2.0f, WHITE);
-            DrawTextureEx(background, (Vector2){ 0, -background.height + bgScroll }, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ 0, bgScroll}, 0.0f, 2.0f, WHITE);
+            DrawTextureEx(background, (Vector2){ 0, -background.height*2 + bgScroll }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(starLayer, (Vector2){ 0, midScroll }, 0.0f, 2.0f, WHITE);
             DrawTextureEx(starLayer, (Vector2){ 0, -starLayer.height + midScroll }, 0.0f, 2.0f, WHITE);
 
