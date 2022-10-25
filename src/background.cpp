cb3654ba2e34dd6f8a339f101a83009f02a38e0e
Sean Gilleece <47053602+Gilleece@users.noreply.github.com>
2022-10-25 16:25:26 +0100

    Updated background, fixed bug with layer

--- src/background.cpp
+++ src/background.cpp
@@ -14,22 +14,22 @@ float foreScroll;       // Scroll speed for foreground layer
 void initBackground() {
     background = LoadTexture("assets/backgrounds/bg1.png");
     midLayer = LoadTexture("assets/backgrounds/ol1.png");
-    Texture2D foreLayer = LoadTexture("assets/backgrounds/ol4.png");
+    foreLayer = LoadTexture("assets/backgrounds/ol4.png");
     bgScroll = 0;
     midScroll = 0;
-    foreScroll = -foreLayer.height;
+    foreScroll = 0;
 }
 
 void updateBackground() {
     //Update background positions
     bgScroll += 5.0f * gameSpeed;
-    midScroll += 10.0f * gameSpeed;
-    foreScroll += 7.5f * gameSpeed;
+    midScroll += 7.5f * gameSpeed;
+    foreScroll += 10.0f * gameSpeed;
 
     //Reset background position to loop
     if (bgScroll >= background.height*2) bgScroll = 0;
-    if (midScroll >= midLayer.height*2) midScroll = -midLayer.height*2;
-    if (foreScroll >= foreLayer.height*2) foreScroll = -foreLayer.height*2;
+    if (midScroll >= midLayer.height*2) midScroll = 0;
+    if (foreScroll >= foreLayer.height*2) foreScroll = 0;
 }
 
 void drawBackground() {
@@ -37,9 +37,9 @@ void drawBackground() {
     DrawTextureEx(background, (Vector2){ 0, bgScroll}, 0.0f, 2.0f, WHITE);
     DrawTextureEx(background, (Vector2){ 0, -background.height*2 + bgScroll }, 0.0f, 2.0f, WHITE);
     DrawTextureEx(midLayer, (Vector2){ 0, midScroll }, 0.0f, 1.0f, WHITE);
-    DrawTextureEx(midLayer, (Vector2){ 0, -midLayer.height + midScroll }, 0.0f, 1.0f, WHITE);
+    DrawTextureEx(midLayer, (Vector2){ 0, -midLayer.height*2 + midScroll }, 0.0f, 1.0f, WHITE);
     DrawTextureEx(foreLayer, (Vector2){ 0, foreScroll }, 0.0f, 2.0f, WHITE);
-    DrawTextureEx(foreLayer, (Vector2){ 0, -foreLayer.height + foreScroll }, 0.0f, 2.0f, WHITE);
+    DrawTextureEx(foreLayer, (Vector2){ 0, -foreLayer.height*2 + foreScroll }, 0.0f, 2.0f, WHITE);
 }
 
 void unloadBackground() {
